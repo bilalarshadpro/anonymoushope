ad62effccbdb5df262698d897f4e56e146b7d7d8
Adeel <madeelakram27@gmail.com>
2021-11-21 16:48:41 +0500

    logIn and signUp linked with database

--- app/src/main/java/com/enfotrix/anonymoushope/LoginActivity.java
+++ app/src/main/java/com/enfotrix/anonymoushope/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.enfotrix.anonymoushope;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,17 +8,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class LoginActivity extends AppCompatActivity {
     private Button btn_login,btn_signUp;
     private EditText edt_phoneNumber,edt_password;
+    FirebaseFirestore db= FirebaseFirestore.getInstance();
+    private Utils Utils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        edt_phoneNumber=findViewById(R.id.edt_phoneNumber);
+        edt_password=findViewById(R.id.edt_password);
         btn_login=findViewById(R.id.btn_login);
         btn_signUp=findViewById(R.id.btn_signUp);
+        Utils=new Utils(this);
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,8 +49,42 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void validate() {
-        Intent intent=new Intent(LoginActivity.this,MainActivity.class);
-        startActivity(intent);
-        finish();
+        if (edt_phoneNumber.getText().toString().isEmpty()){
+            edt_phoneNumber.setError("Username Field is empty");
+        }
+
+        else if(edt_password.getText().toString().isEmpty()){
+            edt_password.setError("Password field is empty");
+        }
+        else{
+            auth(edt_phoneNumber.getText().toString(),edt_password.getText().toString());
+        }
+    }
+
+    private void auth(String str_phoneNo, String str_pass) {
+        db.collection("Users").whereEqualTo("Phone Number",str_phoneNo).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()){
+                    if (!task.getResult().isEmpty()){
+                        for (QueryDocumentSnapshot documentSnapshot: task.getResult()){
+                            String passwordFromDB=documentSnapshot.getString("password");
+                            if (passwordFromDB.equals(str_pass)){
+                                Utils.putToken(str_phoneNo);
+                                Intent intent=new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                            else{
+                                edt_password.setError("Incorrect Password");
+                            }
+                        }
+                    }
+                    else{
+                        edt_phoneNumber.setError("Incorrect Username");
+                    }
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/enfotrix/anonymoushope/SignupActivity.java
+++ app/src/main/java/com/enfotrix/anonymoushope/SignupActivity.java
@@ -1,15 +1,30 @@
 package com.enfotrix.anonymoushope;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignupActivity extends AppCompatActivity {
     private EditText edt_fullName,edt_phoneNo,edt_pass,edt_retypePass;
     private Button btn_save;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +34,7 @@ public class SignupActivity extends AppCompatActivity {
         edt_phoneNo=findViewById(R.id.edt_phoneNum4Signup);
         edt_pass=findViewById(R.id.edt_pass4Signup);
         edt_retypePass=findViewById(R.id.edt_re_pass4signUp);
+        btn_save=findViewById(R.id.btn_saveSignup);
 
         btn_save.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,15 +52,52 @@ public class SignupActivity extends AppCompatActivity {
         }
         else if (edt_pass.getText().toString().isEmpty()){
             edt_pass.setError("Enter Password");
+        }else if (!edt_pass.getText().toString().equals(edt_retypePass.getText().toString())){
+            edt_retypePass.setError("Password does not match");
         }
         else if (edt_pass.getText().toString().length()<8){
-            edt_pass.setError("Password must have 8 character long");
+            edt_pass.setError("Password must be 8 character long");
         }
         else{
             signup(edt_phoneNo.getText().toString().trim());
         }
     }
 
-    private void signup(String edt_phone) {
+    private void signup(String str_phone) {
+        db.collection("Users").whereEqualTo("Phone Number",str_phone).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()){
+                    if (!task.getResult().isEmpty()){
+                        edt_phoneNo.setError("Phone Already exists");
+                    }
+                    else{
+                        String fullName=edt_fullName.getText().toString().trim();
+                        String phoneNumber=edt_phoneNo.getText().toString().trim();
+                        String password=edt_pass.getText().toString().trim();
+
+                        Map<String,String> userMap=new HashMap<>();
+
+                        userMap.put("Full Name",fullName);
+                        userMap.put("Phone Number",phoneNumber);
+                        userMap.put("password",password);
+
+                        db.collection("Users").add(userMap).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Intent intent=new Intent(SignupActivity.this,LoginActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(SignupActivity.this, "Signup failed", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }
+            }
+        });
     }
 }
