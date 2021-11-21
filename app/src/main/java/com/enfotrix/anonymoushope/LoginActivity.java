9b55542b6c499f9e958a6f99c5a32228862943e8
Adeel <madeelakram27@gmail.com>
2021-11-21 16:07:08 +0500

    fixed an error and started working on logIn and signUp

--- app/src/main/java/com/enfotrix/anonymoushope/LoginActivity.java
+++ app/src/main/java/com/enfotrix/anonymoushope/LoginActivity.java
@@ -2,13 +2,42 @@ package com.enfotrix.anonymoushope;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button btn_login,btn_signUp;
+    private EditText edt_phoneNumber,edt_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btn_login=findViewById(R.id.btn_login);
+        btn_signUp=findViewById(R.id.btn_signUp);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate();
+            }
+        });
+        btn_signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void validate() {
+        Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/enfotrix/anonymoushope/SignupActivity.java
+++ app/src/main/java/com/enfotrix/anonymoushope/SignupActivity.java
@@ -3,12 +3,48 @@ package com.enfotrix.anonymoushope;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignupActivity extends AppCompatActivity {
+    private EditText edt_fullName,edt_phoneNo,edt_pass,edt_retypePass;
+    private Button btn_save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        edt_fullName=findViewById(R.id.edt_full_name);
+        edt_phoneNo=findViewById(R.id.edt_phoneNum4Signup);
+        edt_pass=findViewById(R.id.edt_pass4Signup);
+        edt_retypePass=findViewById(R.id.edt_re_pass4signUp);
+
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate();
+            }
+        });
+    }
+
+    private void validate() {
+        if (edt_fullName.getText().toString().isEmpty()){
+            edt_fullName.setError("Enter FullName");
+        }else if (edt_phoneNo.getText().toString().isEmpty()){
+            edt_phoneNo.setError("Enter PhoneNumber");
+        }
+        else if (edt_pass.getText().toString().isEmpty()){
+            edt_pass.setError("Enter Password");
+        }
+        else if (edt_pass.getText().toString().length()<8){
+            edt_pass.setError("Password must have 8 character long");
+        }
+        else{
+            signup(edt_phoneNo.getText().toString().trim());
+        }
+    }
+
+    private void signup(String edt_phone) {
     }
 }
